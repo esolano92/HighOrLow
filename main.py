3fdbc873d60461bbad852fd0d0a5fbabc1df1bd5
esolano92 <78500300+esolano92@users.noreply.github.com>
2023-06-04 17:46:24 -0700

    Update main.py
    added a valid input check and while loop to ensure choice b is always different

--- main.py
+++ main.py
@@ -27,12 +27,15 @@ def interface(choice_A, choice_B):
   
 #scoring_calculation takes on choice's A and B as parameters. Checks of either of them are correct and returns a boolean of True or False
 def scoring_calculation(choice_A, choice_B):
+  valid_inputs = ['a', 'b']
   
   interface(choice_A, choice_B)
   print("Who has the most Instagram followers? ")
   user_pick = input(f"Choose 'A' for {get_name(choice_A)} or choose 'B' for {get_name(choice_B)}: ").lower()
   choice_A_followers = get_follower_count(choice_A)
   choice_B_followers = get_follower_count(choice_B)
+  while user_pick not in valid_inputs:
+    user_pick = input("Invalid selection please select either 'A' or 'B': ")
     
   if user_pick == "a" and choice_A_followers > choice_B_followers:
     
@@ -50,11 +53,15 @@ def scoring_calculation(choice_A, choice_B):
 should_continue = True
 print(logo)
 final_score = 0
+#check if there is enough data to play the game
+if len(data) < 2:
+    print("Insufficient data. The game cannot be played.")
+    should_continue = False
 #while loop that continues to play the game if should_continue is True and increments the final_score varibale. 
 while should_continue: 
   choice_A = random.choice(data)
   choice_B = random.choice(data)
-  if choice_A == choice_B:
+  while choice_A == choice_B:
     choice_B = random.choice(data)
   if scoring_calculation(choice_A,choice_B) == False:
     should_continue = False
@@ -62,7 +69,6 @@ while should_continue:
     final_score += 1
      
 print(f"Your final score is: {final_score}") 
-
 
 
   
